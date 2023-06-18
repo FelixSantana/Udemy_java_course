c8d7779e73007cbe9e3532eaeced114b6fee7382
Felix Santana <flexsantana99@gmail.com>
2023-06-18 19:22:45 -0400

    Subiendo la parte de paquetes

---
+++ Paquetes/src/mx/com/globalmentoring/Utileria.java
@@ -0,0 +1,7 @@
+package Paquetes.src.mx.com.globalmentoring;
+
+public class Utileria {
+    public static void imprimir(String s) {
+        System.out.println("S: " + s);
+    }
+}
---
+++ Paquetes/test/TestUtileria.java
@@ -0,0 +1,9 @@
+package Paquetes.test;
+
+import Paquetes.src.mx.com.globalmentoring.Utileria;
+
+public class TestUtileria {
+    public static void main(String[] args) {
+        Utileria.imprimir("Saludos");
+    }
+}
